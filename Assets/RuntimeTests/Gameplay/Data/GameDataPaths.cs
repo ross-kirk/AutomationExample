c060f08eb1ae11abb0e0d1db26d33f4dd9f701c5
Ross Kirk <rosskirk365@gmail.com>
2025-04-28 18:54:32 +0100

    Add static gameplay data holder

---
+++ Assets/RuntimeTests/Gameplay/Data/GameDataPaths.cs
@@ -0,0 +1,8 @@
+namespace RuntimeTests.Gameplay.Data
+{
+    public static class GameDataPaths
+    {
+        public const string PlayerPrefab = "Prefabs/Player";
+        public const string EnemyPrefab = "Prefabs/Enemy";
+    }
+}
