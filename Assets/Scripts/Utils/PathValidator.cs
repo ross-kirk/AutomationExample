bca80c06b881a74a2feb3b094bd7159d7e0a0606
Ross Kirk <rosskirk365@gmail.com>
2025-05-01 18:34:29 +0100

    Fix path validation to catch correct raycast hits

--- Assets/Scripts/Utils/PathValidator.cs
+++ Assets/Scripts/Utils/PathValidator.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using Platformer.Mechanics;
 using UnityEngine;
 using UnityEngine.Tilemaps;
@@ -38,14 +39,25 @@ namespace Utils
             var samples = Mathf.CeilToInt(Vector2.Distance(start, end));
             var rayLength = enemyHeight + 0.01f;
 
+            var hitBuffer = new List<RaycastHit2D>();
+            var filter = new ContactFilter2D().NoFilter();
+            
             for (var i = 0; i <= samples; i++)
             {
                 var point = Vector2.Lerp(start, end, i / (float) samples);
                 var origin = point + Vector2.up * 0.01f;
+                var hitCount = Physics2D.Raycast(origin, Vector2.down, filter, hitBuffer, rayLength);
 
-                var hit = Physics2D.Raycast(origin, Vector2.down, rayLength);
-                
-                if (hit.collider == null || hit.collider != collider)
+                var hitTilemap = false;
+
+                for (var h = 0; h < hitCount; h++)
+                {
+                    if (hitBuffer[h].collider != collider) continue;
+                    hitTilemap = true;
+                    break;
+                }
+
+                if (!hitTilemap)
                 {
                     Debug.DrawLine(point, origin + Vector2.down * rayLength, Color.yellow, 60f);
                     missingGroundPoints.Add(point);
@@ -54,7 +66,15 @@ namespace Utils
 
             return missingGroundPoints.Count == 0;
         }
-
+        
+        /// <summary>
+        /// Validates an enemy PatrolPath for obstructions & missing ground collision below
+        /// </summary>
+        /// <param name="path">Path to validate</param>
+        /// <param name="collider">Collider to check for</param>
+        /// <param name="onError">Error callback, raised as (path error type, list of affected points in path).</param>
+        /// <param name="enemyHeight">Height of the enemy used for the path (to easily check the ground is close enough to path)</param>
+        /// <returns></returns>
         public static bool ValidatePath(PatrolPath path, TilemapCollider2D collider,  Action<PathValidationError, IReadOnlyList<Vector2>> onError, float enemyHeight = 1f)
         {
             var obstructed = IsObstructed(path, collider, out var obstructedPoints);
